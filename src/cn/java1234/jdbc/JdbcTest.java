4ba7779909bb011e25bd70af59ab0dcc33394602
githubqt <310176154@qq.com>
2016-09-18 23:26:20 +0800

    MyBatis学习，原生jdbc连接数据库和连接池连接数据库

---
+++ src/cn/java1234/jdbc/JdbcTest.java
@@ -0,0 +1,87 @@
+package cn.java1234.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+/**
+ * 	TODO 
+* @author  lqt: 
+* @date 创建时间：2016年5月8日 上午8:43:47 
+* @version 1.0 
+* @parameter  
+* @since  
+* @return  
+*/
+public class JdbcTest {
+	private static JdbcTest JdbcTest;
+	private Connection connection;
+	private String driver = "com.mysql.jdbc.Driver";
+	private String url="jdbc:mysql://localhost/mybatis?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&failOverReadOnly=false";
+	private String userName = "root";
+	private String pwd = "123456";
+
+
+	private JdbcTest(){
+	 try {
+	 Class.forName(driver);		
+	 } catch (ClassNotFoundException e) {
+	 e.printStackTrace();
+	 }catch(Exception e) {
+	 e.printStackTrace();
+	 }
+			
+	 }	
+	
+	public static JdbcTest getInstance(){
+		if(JdbcTest==null){
+			JdbcTest=new JdbcTest();
+		}
+		return JdbcTest;
+	}
+
+	public Connection getConnection() {
+		try {
+			connection = DriverManager.getConnection(url, userName, pwd);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return connection;
+
+	}
+	public void free(ResultSet rs,PreparedStatement pstmt){
+		try {
+			if (rs != null && !rs.isClosed()) {
+				rs.close();
+				rs = null;
+			}
+			
+			if(pstmt!=null&&!pstmt.isClosed()){
+				pstmt.close();
+				pstmt=null;
+			}
+			
+			if(connection!=null&&!connection.isClosed()){
+				connection.close();
+				connection=null;
+			}
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		JdbcTest cn=new JdbcTest();
+		if(cn.getConnection()!=null){
+			System.out.println("数据库连接上了！");
+		}else{
+			System.out.println("数据库连接失败！");
+		}
+	}
+}
---
+++ src/com/java1234/service/StudentTest.java
@@ -0,0 +1,21 @@
+package com.java1234.service;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.java1234.mappers.StudentMapper;
+import com.java1234.model.Student;
+import com.java1234.util.SqlSessionFactoryUtil;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		SqlSession sqlSession=SqlSessionFactoryUtil.openSession();
+		StudentMapper studentMapper=sqlSession.getMapper(StudentMapper.class);
+		Student student=new Student("王二小",12);
+		int result=studentMapper.add(student);
+		sqlSession.commit();
+		if(result>0){
+			System.out.println("添加成功！");
+		}
+	}
+}
