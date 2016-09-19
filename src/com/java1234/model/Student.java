3bf552dad670504d20edeab672c46efd5b1c904a
githubqt <310176154@qq.com>
2016-09-19 22:58:26 +0800

    增删改查

--- src/com/java1234/model/Student.java
+++ src/com/java1234/model/Student.java
@@ -14,13 +14,26 @@ public class Student {
 	}
 	
 	
-	
 	public Student(String name, Integer age) {
 		super();
 		this.name = name;
 		this.age = age;
 	}
+	
+	
+	public Student(Integer id, String name, Integer age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+	
+	
 
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
 
 
 	public Integer getId() {
